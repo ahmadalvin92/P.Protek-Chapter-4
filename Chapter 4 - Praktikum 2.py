5a99f3bb8ffd03282bacf2896b0ea717e9813601
ahmadalvin92 <71621277+ahmadalvin92@users.noreply.github.com>
2020-10-17 11:30:27 +0700

    first commit

---
+++ Chapter 4 - Praktikum 2.py
@@ -0,0 +1,50 @@
+x = 10
+print(type(x))
+y = 20
+print(type(y))
+print(type(x+y))
+
+a=10
+b=10.5
+print(type(a+a))
+print(type(a+b))
+print(type(b+a))
+print(type(b+b))
+
+print(type(a-a))
+print(type(a-b))
+print(type(b-a))
+print(type(b-b))
+
+print(type(a*a))
+print(type(a*b))
+print(type(b*a))
+print(type(b*b))
+
+print(type(a/a))
+print(type(a/b))
+print(type(b/a))
+print(type(b/b))
+
+print(type(a//a))
+print(type(a//b))
+print(type(b//a))
+print(type(b//b))
+
+print(type(a%a))
+print(type(a%b))
+print(type(b%a))
+print(type(b%b))
+
+print(type(a**a))
+print(type(a**b))
+print(type(b**a))
+print(type(b**b))
+
+a = 10
+p = y = x = z = a
+print(a)
+print(z)
+print(x)
+print(y)
+print(p)
---
+++ Latihan 2.py
@@ -0,0 +1,10 @@
+# Variable assignment
+jarak = 795
+
+konsumsiBbm = 12
+
+# Operasi penghitungan
+hasilLiter = jarak/konsumsiBbm
+                       
+# Tampilkan hasil
+print('Total hasil liter bensin yang diperlukan untuk perjalanan : ' + str(hasilLiter))
